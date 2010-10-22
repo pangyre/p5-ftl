a3bf564ce0ef78ff54494809e960acb7262ff447
apv <apv@cassidy.local>
2010-10-21 21:08:39 -0700

    Moved around some things, got a config for the form.

--- lib/FTL/Controller/Scritto.pm
+++ lib/FTL/Controller/Scritto.pm
@@ -1,34 +1,38 @@
-package FTL::Controller::Resource;
+package FTL::Controller::Scritto;
 use Moose;
 use namespace::autoclean;
 
 BEGIN { extends "Catalyst::Controller::HTML::FormFu" }
 
-__PACKAGE__->config(namespace => "r");
+__PACKAGE__->config(namespace => "s");
 
 sub index :Path Args(0) {
     my ( $self, $c ) = @_;
 }
 
-sub load :Chained("/") PathPart("r") CaptureArgs(1) {
-    my ( $self, $c, $name ) = @_;
-    $name ||= $c->request->arguments->[0]; # for forwards
-    $c->stash->{resource} = $c->model("DBIC::Resource")->find($name)
-        || $c->model("DBIC::Resource")->new({ name => $name });
+sub scritto :Chained("/") PathPart("s") CaptureArgs(1) {
+    my ( $self, $c, $title ) = @_;
+    $title ||= $c->request->arguments->[0]; # for forwards
+    $c->stash->{scritto} = $c->model("Scritto")->find($title)
+        || $c->model("Scritto")->new({ title => $title });
 }
 
-sub view :PathPart("") Chained("load") Args(0) {
-    my ( $self, $c ) = @_;
-    my $resource = $c->stash->{resource};
-    $c->detach("default") unless $resource->in_storage;
+sub default :Path {
+
 }
 
-sub edit :Chained("load") Args(0) FormConfig {
+sub view :PathPart("") Chained("scritto") Args(0) {
     my ( $self, $c ) = @_;
-    my $resource = $c->stash->{resource};
+    my $scritto = $c->stash->{scritto};
+    $c->go("default") unless $scritto->in_storage;
+}
+
+sub edit :Chained("scritto") Args(0) FormConfig {
+    my ( $self, $c ) = @_;
+    my $scritto = $c->stash->{scritto};
 
     my $form = $c->stash->{form};
-    $form->constraints_from_dbic($c->model("DBIC::Resource"));
+    $form->constraints_from_dbic($c->model("Scritto"));
     $form->render;
 
     if ( $form->submitted_and_valid )
